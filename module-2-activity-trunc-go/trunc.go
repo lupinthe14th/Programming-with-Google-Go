4f8b8eb327f200848bf069fb39b520a1bf53a065
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-07 07:34:02 +0900

    fix: use math.Trunc
    reviewer say: Just one observation.. it returns the round of figure where question only demands truncation.

--- module-2-activity-trunc-go/trunc.go
+++ module-2-activity-trunc-go/trunc.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	var num float64
-	fmt.Println("Please enter a floating point number.")
+	fmt.Print("Please enter a floating point number: ")
 	fmt.Scan(&num)
-	fmt.Printf("first: %.0f\n", num)
-
-	fmt.Println("Please enter a floating point number.")
-	fmt.Scan(&num)
-	fmt.Printf("second: %.0f\n", num)
+	fmt.Printf("%.0f", math.Trunc(num))
 }
