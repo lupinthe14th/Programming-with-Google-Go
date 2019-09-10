e69dedaa3fe75eeb731c7aba5ae6803a2b185753
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-10 14:17:12 +0900

    feat(module-4-activity-makejson-go): add testcase and improved testability

---
+++ module-4-activity-makejson-go/example_test.go
@@ -0,0 +1,7 @@
+package main
+
+func ExamplePrintPerson() {
+	PrintPerson("taro bando", "Kanto region, Japan")
+	// Output:
+	// {"address":"Kanto region, Japan","name":"taro bando"}
+}
--- module-4-activity-makejson-go/makejson.go
+++ module-4-activity-makejson-go/makejson.go
@@ -4,8 +4,23 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 )
+
+// PrintPerson is a function that print a Person JSON object.
+func PrintPerson(name, address string) error {
+	p := map[string]string{
+		"name":    name,
+		"address": address,
+	}
+	barr, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(barr))
+	return nil
+}
 
 func main() {
 	fmt.Print("Please enter name: ")
@@ -14,10 +29,7 @@ func main() {
 	fmt.Print("Please enter address: ")
 	address := bufio.NewScanner(os.Stdin)
 	address.Scan()
-	user := map[string]string{
-		"name":    name.Text(),
-		"address": address.Text(),
+	if err := PrintPerson(name.Text(), address.Text()); err != nil {
+		log.Fatal(err)
 	}
-	barr, _ := json.Marshal(user)
-	fmt.Println(string(barr))
 }
