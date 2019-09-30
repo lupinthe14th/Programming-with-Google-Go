7158c8e4049618856c7368d3895df498fcdeb22c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-30 17:35:10 +0900

    improve(module-2-activity): refactoring the code

--- golang-functions-methods/module-2-activity/activity.go
+++ golang-functions-methods/module-2-activity/activity.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-// GenDisplaceFn is ...
+// GenDisplaceFn returns a function to compute displacement
 func GenDisplaceFn(a, v0, s0 float64) func(float64) float64 {
 	fn := func(t float64) float64 {
 		return (a*math.Pow(t, 2))/2 + v0*t + s0
@@ -14,18 +14,15 @@ func GenDisplaceFn(a, v0, s0 float64) func(float64) float64 {
 }
 
 func main() {
-	var a, v0, s0, t float64
+	var acc, vel, dis, t float64
 	fmt.Print("Please enter acceleration: ")
-	fmt.Scan(&a)
+	fmt.Scan(&acc)
 	fmt.Print("Please enter initial velocity: ")
-	fmt.Scan(&v0)
+	fmt.Scan(&vel)
 	fmt.Print("Please enter initial displacement: ")
-	fmt.Scan(&s0)
-	fn := GenDisplaceFn(a, v0, s0)
-	fmt.Print("Please enter how many seconds later: ")
+	fmt.Scan(&dis)
+	fn := GenDisplaceFn(acc, vel, dis)
+	fmt.Print("Please enter elapsed: ")
 	fmt.Scan(&t)
-	fmt.Printf("%f seconds later displacement s: %f\n", t, fn(t))
-	fmt.Print("Please enter how many seconds later: ")
-	fmt.Scan(&t)
-	fmt.Printf("%f seconds later displacement s: %f\n", t, fn(t))
+	fmt.Printf("Displacement would be %f after time %f with given acceleration %f, initial velocity %f and initial displacement %f\n", fn(t), t, acc, vel, dis)
 }
--- golang-functions-methods/module-2-activity/activity_test.go
+++ golang-functions-methods/module-2-activity/activity_test.go
@@ -12,21 +12,21 @@ type initial struct {
 var cases = []struct {
 	id   int
 	init initial
-	t    []float64
-	want []float64
+	t    float64
+	want float64
 }{
-	{id: 1, init: initial{a: 10, v0: 2, s0: 1}, t: []float64{0, 3, 5}, want: []float64{1, 52, 136}},
+	{id: 1, init: initial{a: 10, v0: 2, s0: 1}, t: 0, want: 1},
+	{id: 2, init: initial{a: 10, v0: 2, s0: 1}, t: 3, want: 52},
+	{id: 3, init: initial{a: 10, v0: 2, s0: 1}, t: 5, want: 136},
 }
 
 func TestGen(t *testing.T) {
 	for _, tt := range cases {
 		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
 			fn := GenDisplaceFn(tt.init.a, tt.init.v0, tt.init.s0)
-			for i, at := range tt.t {
-				got := fn(at)
-				if got != tt.want[i] {
-					t.Errorf("%f, want: %f", got, tt.want[i])
-				}
+			got := fn(tt.t)
+			if got != tt.want {
+				t.Errorf("%f, want: %f", got, tt.want)
 			}
 		})
 	}
