c2e9d08c804824d65d04f02e941eb3d4158021d9
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-12 21:11:01 +0900

    fix(golang-concurrency/module-2-activity): first submit version
    update: add explanation of race conditions.

--- golang-concurrency/module-2-activity/racecondition.go
+++ golang-concurrency/module-2-activity/racecondition.go
@@ -1,3 +1,7 @@
+// # What's race condition?
+// A race condition occurs when two or more operations must execute in the correct order, but the program has not been written so that this order is guaranteed to be maintained.
+// Most of the time, A race condition shows up in what's called a data race, where one concurrent operation attempts to read a variable while at some undermined time another concurrent operation is attempting to write to the same variable.
+// - Katherine Cox-Buday (2017) Concurrency in Go, O'Reilly Media, Inc.
 package main
 
 import (
