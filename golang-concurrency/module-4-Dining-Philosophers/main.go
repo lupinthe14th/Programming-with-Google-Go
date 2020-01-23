c68745b7814e127d06f0ec086c6b03f8405a78d8
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-23 16:43:06 +0900

    improve(golang-concurrency/module-4-Dining-Philosophers): Module 4 Activity

--- golang-concurrency/module-4-Dining-Philosophers/main.go
+++ golang-concurrency/module-4-Dining-Philosophers/main.go
@@ -1,3 +1,4 @@
+// Refarence: http://rosettacode.org/wiki/Dining_philosophers#Mutexes_and_WaitGroup
 package main
 
 import (
@@ -12,40 +13,45 @@ type ChopS struct {
 
 // Philo is Philosophers structure
 type Philo struct {
+	number          int
 	leftCS, rightCS *ChopS
 }
 
+// In order to eat, a philosopher must get permission from a host which executes in its own goroutine.
+var wg sync.WaitGroup
+
 // Philosopher Eat Method
 func (p Philo) eat() {
-	for {
+	for i := 0; i < 3; i++ {
 		p.leftCS.Lock()
 		p.rightCS.Lock()
 
-		fmt.Println("eating")
+		fmt.Printf("starting to eat %d\n", p.number)
+		fmt.Printf("finishing eating %d\n", p.number)
 
 		p.rightCS.Unlock()
-
 		p.leftCS.Unlock()
-
 	}
+	wg.Done()
 }
 
 func main() {
 	// Initialization
-	CSticks := make([]*ChopS, 5)
-
-	for i := 0; i < 5; i++ {
-		CSticks[i] = new(ChopS)
-	}
-
+	wg.Add(2) // The host allows no more than 2 philosophers to eat concurrently.
+	CStick0 := new(ChopS)
+	CStickLeft := CStick0
 	philos := make([]*Philo, 5)
-
-	for i := 0; i < 5; i++ {
-		philos[i] = &Philo{CSticks[i], CSticks[(i+1)%5]}
+	for i := 1; i < 5; i++ {
+		CStickRight := new(ChopS)
+		philos[i] = &Philo{i + 1, CStickLeft, CStickRight}
+		CStickLeft = CStickRight
 	}
+	philos[0] = &Philo{1, CStick0, CStickLeft}
 
 	// Start the Dining
 	for i := 0; i < 5; i++ {
 		go philos[i].eat()
 	}
+	wg.Wait()
+
 }
